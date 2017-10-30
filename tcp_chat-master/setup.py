84e5846717f481c608e65f4f0de56261882c9280
Sunil Yadav <sunilyadav747884@gmail.com>
2017-10-30 19:16:36 +0530

    Update setup.py

--- tcp_chat-master/setup.py
+++ tcp_chat-master/setup.py
@@ -13,7 +13,7 @@ setup(
 	license='MIT',
 	packages = ['tcp_chat'],
 	install_requires = ['clint'],
-	download_url = 'https://github.com/pdhoot/tcp_chat/tarball/0.1' ,
+	download_url = 'https://github.com/Sunil8239/tcp_chatapp/tarball/0.1' ,
 	keywords = ['chat' , 'tcp-chat'],
 	entry_points = {
 		'console_scripts' : ['tcp_chat = tcp_chat.tcp_chat:main']
